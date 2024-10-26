0f929d6fc4afcdfbc341be16397f33f9c7d0e91d
TorOdinson14 <konyak71442@gmail.com>
2024-10-26 22:49:41 +1100

    qwe

--- js/menu.js
+++ js/menu.js
@@ -9,7 +9,8 @@ function closeNav() {
 }
 
 // Функция для загрузки содержимого
-function loadContent(page) {
+function loadContent(element) {
+    const page = element.getAttribute('data-page'); // Получаем имя файла из data-page
     fetch(page)
         .then(response => {
             if (!response.ok) {
@@ -37,20 +38,4 @@ document.addEventListener('DOMContentLoaded', () => {
 function toggleSubmenu() {
     const submenu = document.getElementById('submenu');
     submenu.style.display = submenu.style.display === 'block' ? 'none' : 'block';
-}
-function loadContent(page) {
-    fetch(page)
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
-            return response.text();
-        })
-        .then(data => {
-            document.getElementById('Theme').innerHTML = data;
-        })
-        .catch(error => {
-            console.error('There has been a problem with your fetch operation:', error);
-            document.getElementById('Theme').innerHTML = '<p>Ошибка загрузки содержимого.</p>';
-        });
 }
