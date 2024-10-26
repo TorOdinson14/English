b1aece02f83cfa8dc28e647368562bb2f9c6bd61
TorOdinson14 <konyak71442@gmail.com>
2024-10-26 22:25:38 +1100

    qwe

--- js/menu.js
+++ js/menu.js
@@ -38,3 +38,19 @@ function toggleSubmenu() {
     const submenu = document.getElementById('submenu');
     submenu.style.display = submenu.style.display === 'block' ? 'none' : 'block';
 }
+function loadContent(page) {
+    fetch(page)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.text();
+        })
+        .then(data => {
+            document.getElementById('Theme').innerHTML = data;
+        })
+        .catch(error => {
+            console.error('There has been a problem with your fetch operation:', error);
+            document.getElementById('Theme').innerHTML = '<p>Ошибка загрузки содержимого.</p>';
+        });
+}
